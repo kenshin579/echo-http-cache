be3be1786b20361c065816a8660ee68f3b847911
kenshin579 <kenshin579@hotmail.com>
2023-07-08 22:09:24 +0900

    [#5] adding unit test for testing echo middleware with redis store

--- redis_test.go
+++ redis_test.go
@@ -2,41 +2,54 @@ package echo_http_cache
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/alicebob/miniredis/v2"
 	"github.com/go-redis/redis/v8"
 	"github.com/kenshin579/echo-http-cache/test"
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/suite"
 )
 
-type redisStoreTestSuite struct {
+type cacheRedisStoreTestSuite struct {
 	suite.Suite
 	ctx       context.Context
 	miniredis *miniredis.Miniredis
 
 	cacheStore CacheStore
+	echo       *echo.Echo
 }
 
-func TestRedisStoreSuite(t *testing.T) {
-	suite.Run(t, new(redisStoreTestSuite))
+func TestCacheRedisStoreSuite(t *testing.T) {
+	suite.Run(t, new(cacheRedisStoreTestSuite))
 }
-func (suite *redisStoreTestSuite) SetupSuite() {
+func (suite *cacheRedisStoreTestSuite) SetupSuite() {
 	suite.ctx = context.Background()
+
 	db, _ := test.NewRedisDB()
 	store := NewCacheRedisStoreWithConfig(redis.Options{
 		Addr: db.Addr(),
 	})
 	suite.miniredis = db
 	suite.cacheStore = store
+
+	suite.echo = echo.New()
+	suite.echo.Use(CacheWithConfig(CacheConfig{
+		Store:        store,
+		expiration:   5 * time.Second,
+		includePaths: []string{"/test"},
+	}))
 }
 
-func (suite *redisStoreTestSuite) TearDownSuite() {
+func (suite *cacheRedisStoreTestSuite) TearDownSuite() {
 	suite.miniredis.Close()
 }
 
-func (suite *redisStoreTestSuite) Test() {
+func (suite *cacheRedisStoreTestSuite) Test_Redis_CacheStore() {
 	key := generateKey("GET", "1")
 
 	suite.Run("Set, Get", func() {
@@ -51,8 +64,32 @@ func (suite *redisStoreTestSuite) Test() {
 	suite.Run("Delete", func() {
 		suite.cacheStore.Release(key)
 		_, ok := suite.cacheStore.Get(key)
-
 		suite.False(ok)
 
+	})
+}
+
+func (suite *cacheRedisStoreTestSuite) Test_Echo_CacheWithConfig() {
+	suite.echo.GET("/test", func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	})
+
+	suite.Run("echo cache test with redis store", func() {
+		req := httptest.NewRequest(http.MethodGet, "/test", nil)
+		rec := httptest.NewRecorder()
+
+		suite.echo.ServeHTTP(rec, req)
+
+		suite.Equal(http.StatusOK, rec.Code)
+		suite.Equal("test", rec.Body.String())
+
+		key := generateKey(http.MethodGet, "/test")
+		data, ok := suite.cacheStore.Get(key)
+		suite.True(ok)
+
+		var cacheResponse CacheResponse
+		err := json.Unmarshal(data, &cacheResponse)
+		suite.NoError(err)
+		suite.Equal("test", string(cacheResponse.Value))
 	})
 }
